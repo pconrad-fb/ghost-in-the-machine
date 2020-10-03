010bc791e7c89b27b7952ee558513054e91ac0bc
Peter Conrad <stymied@gmail.com>
2020-10-02 20:15:32 -0700

    Added color guess, minimum viable product is up.

--- gitm.js
+++ gitm.js
@@ -9,6 +9,12 @@ window.g = {
 	gitm_chat_history: "",
 };
 
+function reset(){
+	setCookie("gitm_visited_nodes", "");
+	setCookie("gitm_solved_contexts", "");
+	setCookie("gitm_game_finished", "no");
+	setCookie("gitm_current_node", "start");
+}
 
 function setCookie(parameter, value) {
 	document.cookie = parameter + "=" + value + "; SameSite=Lax";
@@ -27,18 +33,41 @@ function getCookie(parameter) {
   return "";
 }
 
+function postReply(response){
+	let chatHistoryDiv = document.getElementById("chatHistory");
+	response = '<br /><span class="computer">' + response + '</span>';
+	g.gitm_chat_history = chatHistoryDiv.innerHTML + response;
+	// Trim to the last 4000 characters of chat history so it will fit in a cookie
+	if(g.gitm_chat_history.length > 4000){
+		g.gitm_chat_history = g.gitm_chat_history.substr(g.gitm_chat_history.length-4000);
+		g.gitm_chat_history = g.gitm_chat_history.substr(g.gitm_chat_history.indexOf("</span>")+7);
+	}
+	chatHistoryDiv.innerHTML = g.gitm_chat_history;
+	chatHistoryDiv.scrollTop = chatHistoryDiv.scrollHeight;
+}
+
+function chatReply(input){
+	if(g.gitm_current_node === "end"){
+		//if (hasVisited(nodes.end){
+			//postReply(nodes[g.gitm_current_node].greeting);
+	}else{
+	  postReply(buildResponse(input));
+  }
+}
 
 function setup(){
+	let chatHistoryDiv = document.getElementById("chatHistory");
   console.log("Setup\n");
 	// if we have no existing cookie
 	if (getCookie("gitm_game_finished") === ""){
+		console.log("No cookies.");
 		//alert("No cookie");
 		g.gitm_game_finished = "no";
 		setCookie("gitm_game_finished", g.gitm_game_finished);
 		g.gitm_current_node = "start";
 		setCookie("gitm_current_node", g.gitm_current_node);
 	}else{
-		//alert("Cookies!");
+		console.log("Reading in cookies.");
 		// g.gitm_visited_nodes: [],
 		g.gitm_visited_nodes = getCookie("gitm_visited_nodes").split(",");
 		// g.gitm_solved_contexts: [],
@@ -47,12 +76,12 @@ function setup(){
 		g.gitm_game_finished = getCookie("gitm_game_finished");
 		// g.gitm_current_node: node name
 		g.gitm_current_node = getCookie("gitm_current_node");
-		// g.gitm_chat_history: the last 4000 bytes of the chat
-		g.gitm_chat_history = getCookie("gitm_chat_history");
-		console.log ("g.gitm_game_finished: " + g.gitm_game_finished);
+		console.log (g);
 	}
 	if(g.gitm_current_node === "start"){
-		postReply(start.greeting);
+		postReply(nodes.start.greeting);
+	}else{
+		postReply(nodes[g.gitm_current_node].greeting);
 	}
 }
 
@@ -65,10 +94,13 @@ function hasVisited(node){
 	}
 }
 
-
 function isSolved(context){
 	console.log("Solved " + context + "?");
-	return false;
+	if(g.gitm_solved_contexts.includes(context)){
+		return true;
+	}else{
+		return false;
+	}
 }
 
 function pickFromGroup(phraseGroup){
@@ -87,6 +119,7 @@ function getPhrase(node){
 		// push it
 		g.gitm_visited_nodes.push(node);
 		console.log(g.gitm_visited_nodes);
+		setCookie("gitm_visited_nodes", g.gitm_visited_nodes.join(","));
 		// Use the greeting
 		return nodes[node].greeting;
 	}
@@ -94,11 +127,14 @@ function getPhrase(node){
 
 function processInput(e) {
   e.preventDefault();
-	if(g.gitm_game_finished === "yes"){
+	g.playerInput = document.getElementById("inputText").value;
+	if(g.playerInput === "reset gitm"){
+		reset();
+		setup();
+	} else if (g.gitm_game_finished === "yes"){
 		return;
 	}
-	g.playerInput = document.getElementById("inputText").value;
-	let inputHTML = '<br /><span class="player">' + g.playerInput + '</span>';
+let inputHTML = '<br /><span class="player">' + g.playerInput + '</span>';
 	let chatHistoryDiv = document.getElementById("chatHistory");
 	let chat = chatHistoryDiv.innerHTML + inputHTML;
 	chatHistoryDiv.innerHTML = chat;
@@ -114,15 +150,15 @@ function processInput(e) {
 // in one node, but often leave it in another.
 
 function buildResponse(input){
-	console.log("Currently in " + g.gitm_current_node);
+	console.log(g);
 	let just_started, detected = false;
 	// Process the very first input
 	if(g.gitm_current_node === "start"){
-		g.gitm_current_node = contexts[start.next_context].first_node;
+		g.gitm_current_node = contexts[nodes.start.next_context].first_node;
 		just_started = true;
 	}
 	let reply = "ERROR - no reply!";
-	let current = nodes[g.gitm_current_node].context;
+	current = nodes[g.gitm_current_node].context;
 	input = input.toLowerCase();
 
 	// Check for keywords...
@@ -138,13 +174,20 @@ function buildResponse(input){
 		}
 	}
 	console.log("input: " + input);
-	// console.log("keyword: " + keyword);
+	if(input === "start_over"){
+		reset();
+		location.reload(false);
+		//return start.greeting;
+	}
 
 	// Did player solve the context? Change nodes
-	if(input === contexts[nodes[g.gitm_current_node].context].exit){
+	if(input === contexts[current].exit){
+		g.gitm_solved_contexts.push(current);
+		setCookie("gitm_solved_contexts", g.gitm_solved_contexts.join(","));
 		console.log("Matched " + contexts[nodes[g.gitm_current_node].context].exit);
 		current = contexts[current].next_context;
 		g.gitm_current_node = contexts[current].first_node;
+		setCookie("gitm_current_node", g.gitm_current_node);
 		//setCookie("gitm_current_node", g.gitm_current_node);
 		//alert(g.gitm_current_node);
 		// If this one's already solved, something's wrong
@@ -153,17 +196,13 @@ function buildResponse(input){
 		}
 		//alert("Solved! Moved to " + g.gitm_current_node);
 		if(g.gitm_current_node === "end"){
-			// alert("End game!");
-			// do the end stuff
-			// update the cookies
 			g.gitm_game_finished = "yes";
-			return end.greeting;
+			return nodes.end.greeting;
 		}
 	}
 
 	// Keyword moves us to a new node? But not if context solved.
 	let move_to_node;
-
 	for (keyword in nodes[g.gitm_current_node].exits){
 		if(input === keyword){
 			detected = true;
@@ -174,23 +213,26 @@ function buildResponse(input){
 			// and return something from that context's sink
 			if(isSolved(nodes[move_to_node].context)){
 				console.log( "New node's context already solved. Not moving, picking from sink.");
-				reply = pickFromGroup(nodes[move_to_node].sink);
+				reply = pickFromGroup(contexts[nodes[move_to_node].context].sink);
 				break;
 			}else {
 				// Otherwise, move
 				g.gitm_current_node = move_to_node;
 				console.log("Moved to: " + g.gitm_current_node);
+				setCookie("gitm_current_node", g.gitm_current_node);
 				reply = getPhrase(g.gitm_current_node);
 				//return getPhrase(g.gitm_current_node);
 			}
 		}
-	} // for
+	} // for loop
 
 	if(!detected){
 		// If it's the first time in after the start node, no keyword
 		// means do the greeting
 			if(just_started){
 				reply = nodes[g.gitm_current_node].greeting;
+				g.gitm_visited_nodes.push(g.gitm_current_node);
+				setCookie("gitm_visited_nodes", g.gitm_visited_nodes.join(","));
 			}else{
 				// No keyword match, don't go anywhere, grab from default
 				reply = getPhrase(nodes[g.gitm_current_node].default);
@@ -198,29 +240,4 @@ function buildResponse(input){
 	}
 
 	return reply;
-
-}
-
-
-function postReply(response){
-	let chatHistoryDiv = document.getElementById("chatHistory");
-	response = '<br /><span class="computer">' + response + '</span>';
-	g.gitm_chat_history = chatHistoryDiv.innerHTML + response;
-	// Trim to the last 4000 characters of chat history so it will fit in a cookie
-	if(g.gitm_chat_history.length > 4000){
-		g.gitm_chat_history = g.gitm_chat_history.substr(g.gitm_chat_history.length-4000);
-		g.gitm_chat_history = g.gitm_chat_history.substr(g.gitm_chat_history.indexOf("</span>")+7);
-	}
-	chatHistoryDiv.innerHTML = g.gitm_chat_history;
-	chatHistoryDiv.scrollTop = chatHistoryDiv.scrollHeight;
-	setCookie("gitm_chat_history", g.gitm_chat_history);
-}
-
-function chatReply(input){
-	if(g.gitm_current_node === "end"){
-		//if (hasVisited(nodes.end){
-			//postReply(nodes[g.gitm_current_node].greeting);
-	}else{
-	  postReply(buildResponse(input));
-  }
 }
